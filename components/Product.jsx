1653c097ea926e9e0c619e667565710889cf05a3
Jasem Nazeri <78740057+justimnax@users.noreply.github.com>
2022-09-08 11:20:26 +0430

    Update Product.jsx

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { urlFor } from '../lib/client';
+// fetching through props
 const Product = ({ product: { image, name, slug, price } }) => {
   return (
     <div>
