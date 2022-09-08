2e08926f203bcd9abac4a145fbcca0f226b967cb
Jasem Nazeri <78740057+justimnax@users.noreply.github.com>
2022-09-08 11:21:21 +0430

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,6 +6,7 @@ import { Cart } from './';
 import { useStateContext } from '../context/StateContext';
 
 const Navbar = () => {
+  //fetching context
   const { showCart, setShowCart, totalQuantities } = useStateContext();
 
   return (
