f8262cb6e793505256f3915a387727cea1ef5a4c
Jasem Nazeri <78740057+justimnax@users.noreply.github.com>
2022-09-09 21:18:04 +0430

    Update Cart.jsx

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -124,7 +124,7 @@ const Cart = () => {
         {cartItems.length >= 1 && (
           <div className='cart-bottom'>
             <div className='total'>
-              <h3>Subtotal:</h3>
+              <h1>Subtotal:</h1>
               <h3>${totalPrice}</h3>
             </div>
             <div className='btn-container'>
