a3dfa26ce0007800ba9da2b75693f9b5500f841c
Jasem Nazeri <78740057+justimnax@users.noreply.github.com>
2022-12-15 11:54:17 +0330

    Update index.js

--- components/index.js
+++ components/index.js
@@ -5,3 +5,4 @@ export { default as HeroBanner } from './HeroBanner';
 export { default as Navbar } from './Navbar';
 export { default as Product } from './Product';
 export { default as FooterBanner } from './FooterBanner';
+// exports all modules
